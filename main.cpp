8491d86a8c00dae2fd19220cef40c656450fd3f4
zetcc0 <carabellochinellato.juanpablo@gmail.com>
2026-05-30 20:02:44 -0300

    testing hook, works fine

--- main.cpp
+++ main.cpp
@@ -3,31 +3,30 @@
 #include <stdio.h>
 #include <TlHelp32.h>
 #include "hook.h"
-#define LOGFILE "C:\\Users\\carab\\Desktop\\pinball3d\\patcher\\hook_calls.txt"
+#define LSBILOGFILE "C:\\Users\\carab\\Desktop\\pinball3d\\patcher\\lsbilf.txt"
+#define PLOGFILE "C:\\Users\\carab\\Desktop\\pinball3d\\patcher\\plf.txt"
 
 
 static CRITICAL_SECTION cs;          // guards file access
-static void LogToFile(const char* msg)
+static void LogToFile(const char* msg, LPCSTR path)
 {
     EnterCriticalSection(&cs);
-    FILE* f = fopen(LOGFILE, "a");
+    FILE* f = fopen(path, "a");
     if (f) { fprintf(f, "%s\n", msg); fclose(f); }
     LeaveCriticalSection(&cs);
 }
 
-typedef LPSTR (__stdcall *func_t)(short, DWORD);
-func_t Trampoline = nullptr;
-
-LPSTR __stdcall Hook(short id, DWORD unused)
+LPSTR (__stdcall *LoadStrByIdTrampoline)(short, DWORD) = nullptr;
+LPSTR __stdcall LoadStrById(short id, DWORD unused)
 {
     char buf[256];
-    wsprintfA(buf, "Called with id = %d", id);
-    LogToFile(buf);
-    LPSTR result = Trampoline(id, unused);
+    wsprintfA(buf, "Called LoadStrById with id = %d", id);
+    LogToFile(buf,LSBILOGFILE);
+    LPSTR result = LoadStrByIdTrampoline(id, unused);
 
     if (id == 50) 
     {
-      lstrcpynA(result, "boringgggg", 256); 
+      lstrcpynA(result, "boringg", 256); 
     }
     else if (id == 38) 
     {
@@ -41,13 +40,35 @@ LPSTR __stdcall Hook(short id, DWORD unused)
         strncpy(copy, result, 511);
         copy[511] = '\0';
         wsprintfA(log, "Returned: \"%s\"", copy);
-        LogToFile(log);
+        LogToFile(log, LSBILOGFILE);
     }
-    else { LogToFile("Returned NULL"); }
+    else { LogToFile("Returned NULL", LSBILOGFILE); }
 
     
     return result;
 }
+
+
+int (__stdcall *physicsTrampoline)(int) = nullptr;
+int __stdcall physics(int id)
+{
+    char buf[256];
+    wsprintfA(buf, "Called physics with id = %d", id);
+    LogToFile(buf, PLOGFILE);
+    int result = physicsTrampoline(id);
+
+    if (result)
+    {
+        char log[512];
+        wsprintfA(log, "Returned: \"%d\"", result);
+        LogToFile(log, PLOGFILE);
+    }
+    else { LogToFile("Returned NULL", PLOGFILE); }
+
+    
+    return result;
+}
+
 
 // ---------- DLL entry point ----------
 BOOL WINAPI DllMain(HINSTANCE h, DWORD r, LPVOID)
@@ -56,8 +77,11 @@ BOOL WINAPI DllMain(HINSTANCE h, DWORD r, LPVOID)
     {
         DisableThreadLibraryCalls(h);
         InitializeCriticalSection(&cs);
-        DeleteFileA(LOGFILE);
-        InstallHook(0x3752, 5, (void*)&Hook, (void**)&Trampoline);
+        DeleteFileA(LSBILOGFILE);
+        DeleteFileA(PLOGFILE);
+        InstallHook(0x3752, 5, (void*)&LoadStrById, (void**)&LoadStrByIdTrampoline);
+        InstallHook(0x14bf9, 5, (void*)&physics, (void**)&physicsTrampoline); //14bf9
+
     }
     return TRUE;
 }
